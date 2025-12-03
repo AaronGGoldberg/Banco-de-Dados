0c49eee94cb36c21d869b7486a7ec505283c7205
Aaron Goldberg <aaron.guerra137@gmail.com>
2025-12-03 18:25:51 +0000

    Filtrando resultado de uma tabela através de string

--- ResumoSlides.sql
+++ ResumoSlides.sql
@@ -106,3 +106,6 @@ DELETE from dbo.cliente WHERE email is NULL;
 
 DELETE from dbo.cidade
 	WHERE nome like '% Machado' or nome LIKE '% Machado %';
+
+SELECT * from dbo.cliente
+	WHERE nome like '_%a'
